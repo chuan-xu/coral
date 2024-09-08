37dd042fb803f58a017a755132e0c18799a06884
kiljiaden <kiljiaden@163.com>
2024-09-08 14:38:22 +0800

    add proxy benchmark

--- coral-proxy/tests/client.rs
+++ coral-proxy/tests/client.rs
@@ -5,8 +5,10 @@ use std::sync::Arc;
 
 use bytes::Buf;
 use bytes::BufMut;
+use bytes::Bytes;
 use coral_runtime::tokio;
 use futures::future::join_all;
+use h3::client::SendRequest;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -33,6 +35,44 @@ enum Error {
     InvalidData,
 }
 
+async fn request(mut sender: SendRequest<h3_quinn::OpenStreams, Bytes>) -> Result<(), Error> {
+    let req = hyper::http::Request::builder()
+        .method("POST")
+        // .uri("https://server.test.com:9001/benchmark")
+        .uri("https://tx.coral.com:9001/benchmark")
+        .header(hyper::header::CONTENT_LENGTH, "36")
+        // .version(hyper::Version::HTTP_3)
+        .body(())?;
+    let mut stream = sender.send_request(req).await?;
+    stream
+        .send_data(bytes::Bytes::from_static(b"1234567890"))
+        .await?;
+    stream
+        .send_data(bytes::Bytes::from_static(b"qwertyuiop"))
+        .await?;
+    stream
+        .send_data(bytes::Bytes::from_static(b"asdfghjkl"))
+        .await?;
+    stream
+        .send_data(bytes::Bytes::from_static(b"zxcvbnm"))
+        .await?;
+    stream.finish().await?;
+    let resp = stream.recv_response().await?;
+    if resp.status() != hyper::StatusCode::OK {
+        return Err(Error::StatusErr);
+    }
+    let mut buf = bytes::BytesMut::with_capacity(1024).writer();
+    while let Some(chunk) = stream.recv_data().await? {
+        buf.write(chunk.chunk())?;
+    }
+    let data = buf.into_inner().freeze();
+    if data != "1234567890qwertyuiopasdfghjklzxcvbnm" {
+        return Err(Error::InvalidData);
+    } else {
+        Ok(())
+    }
+}
+
 async fn handle(conf: quinn::ClientConfig, addr: SocketAddr) -> Result<(), Error> {
     let mut endpoints = quinn::Endpoint::client("[::]:0".parse().unwrap()).unwrap();
     endpoints.set_default_client_config(conf);
@@ -43,51 +83,22 @@ async fn handle(conf: quinn::ClientConfig, addr: SocketAddr) -> Result<(), Error
         .await
         .unwrap();
     let quinn_conn = h3_quinn::Connection::new(conn);
-    let (mut driver, mut send_request) = h3::client::new(quinn_conn).await.unwrap();
+    let (mut driver, sender) = h3::client::new(quinn_conn).await.unwrap();
     let drive = async move {
         futures::future::poll_fn(|cx| driver.poll_close(cx))
             .await
             .unwrap();
     };
-    let request = async move {
-        let req = hyper::http::Request::builder()
-            .method("POST")
-            // .uri("https://server.test.com:9001/benchmark")
-            .uri("https://tx.coral.com:9001/benchmark")
-            .header(hyper::header::CONTENT_LENGTH, "36")
-            // .version(hyper::Version::HTTP_3)
-            .body(())?;
-        let mut stream = send_request.send_request(req).await?;
-        stream
-            .send_data(bytes::Bytes::from_static(b"1234567890"))
-            .await?;
-        stream
-            .send_data(bytes::Bytes::from_static(b"qwertyuiop"))
-            .await?;
-        stream
-            .send_data(bytes::Bytes::from_static(b"asdfghjkl"))
-            .await?;
-        stream
-            .send_data(bytes::Bytes::from_static(b"zxcvbnm"))
-            .await?;
-        stream.finish().await?;
-        let resp = stream.recv_response().await?;
-        if resp.status() != hyper::StatusCode::OK {
-            return Err(Error::StatusErr);
-        }
-        let mut buf = bytes::BytesMut::with_capacity(1024).writer();
-        while let Some(chunk) = stream.recv_data().await? {
-            buf.write(chunk.chunk())?;
-        }
-        let data = buf.into_inner().freeze();
-        if data != "1234567890qwertyuiopasdfghjklzxcvbnm" {
-            return Err(Error::InvalidData);
-        } else {
-            Ok(())
-        }
-    };
-    let (req_res, _drive_res) = tokio::join!(request, drive);
-    req_res?;
+    tokio::spawn(drive);
+    for _ in 0..500 {
+        let sender = sender.clone();
+        tokio::spawn(async move {
+            if let Err(err) = request(sender).await {
+                println!("{:?}", err);
+            }
+        });
+    }
+    drop(sender);
     endpoints.wait_idle().await;
     Ok(())
 }
@@ -114,7 +125,7 @@ async fn parallel() {
         // 9001,
     );
     let mut tasks = vec![];
-    for _ in 0..100 {
+    for _ in 0..20 {
         let conf = client_config.clone();
         let addr = addr.clone();
         tasks.push(tokio::spawn(async move {
@@ -129,7 +140,7 @@ async fn parallel() {
 #[test]
 fn run() {
     let rt = tokio::runtime::Builder::new_multi_thread()
-        .worker_threads(2)
+        .worker_threads(3)
         .enable_all()
         .build()
         .unwrap();
