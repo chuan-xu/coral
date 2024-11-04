b0f5134e3f18b0e5cdb9a2704dcb86d15802bcb7
kiljiaden <kiljiaden@163.com>
2024-11-04 23:04:13 +0800

    update captcha

--- coral-captcha/src/lib.rs
+++ coral-captcha/src/lib.rs
@@ -1,7 +1,7 @@
 //! generate captcha
 
 use image::ImageResult;
-use std::path::Path;
+use std::{path::Path, str::Chars};
 
 /// 文字点选验证码
 #[derive(Default)]
@@ -10,7 +10,7 @@ pub struct TxtClickCaptchaBuilder {
     total_chars: Vec<char>,
 
     /// 需要点选的文字顺序
-    sel_chars: Vec<char>,
+    sel_chars: Vec<usize>,
 
     /// 文字块大小
     txt_size: u32,
@@ -27,17 +27,24 @@ impl TxtClickCaptchaBuilder {
         Self::default()
     }
 
-    pub fn set_total_chars(mut self, chars: &[char]) -> Self {
+    pub fn set_total_chars(mut self, chars: Chars) -> Self {
         for c in chars {
             self.total_chars.push(c.to_owned())
         }
-        // self.txt_nums = n;
         self
     }
 
-    pub fn set_sel_chars(mut self, n: u8) -> Self {
-        // self.sel_nums = n;
-        self
+    pub fn set_sel_chars(mut self, sel: &[usize]) -> CaptchaResult<Self> {
+        if sel.len() > self.total_chars.len() {
+            return Err(CaptchaError::InvalidTxtNums);
+        }
+        for s in sel {
+            if *s > self.total_chars.len() {
+                return Err(CaptchaError::InvalidTxtNums);
+            }
+            self.sel_chars.push(*s);
+        }
+        Ok(self)
     }
 
     pub fn set_txt_size(mut self, size: u32) -> Self {
@@ -55,9 +62,6 @@ impl TxtClickCaptchaBuilder {
         if self.txt_size > self.box_width || self.txt_size > self.box_height {
             return Err(CaptchaError::InvalidTxtSize);
         }
-        // if self.txt_nums < self.sel_nums {
-        //     return Err(CaptchaError::InvalidTxtNums);
-        // }
         Ok(())
     }
 
@@ -68,6 +72,7 @@ impl TxtClickCaptchaBuilder {
         let buf = buf.resize_exact(width, self.box_height, image::imageops::FilterType::Nearest);
         Ok(TxtClickCaptcha {
             total_chars: self.total_chars,
+            sel_chars: self.sel_chars,
             txt_size: self.txt_size,
             box_width: self.box_width,
             box_height: self.box_height,
@@ -83,6 +88,7 @@ impl TxtClickCaptchaBuilder {
         let buf = image::DynamicImage::new_rgba8(width, self.box_height);
         Ok(TxtClickCaptcha {
             total_chars: self.total_chars,
+            sel_chars: self.sel_chars,
             txt_size: self.txt_size,
             box_width: self.box_width,
             box_height: self.box_height,
@@ -99,7 +105,7 @@ pub struct TxtClickCaptcha {
     total_chars: Vec<char>,
 
     /// 需要点选的文字顺序
-    // sel_chars: Vec<char>,
+    sel_chars: Vec<usize>,
 
     /// 文字块大小
     txt_size: u32,
@@ -120,6 +126,14 @@ pub struct TxtClickCaptcha {
     buf: image::DynamicImage,
 }
 
+impl TxtClickCaptcha {
+    pub fn save(self) {
+        // self.buf.save_with_format(, )
+        // self.buf.save_with_format(, )
+        // self.buf.into_bytes()
+    }
+}
+
 type CaptchaResult<T> = Result<T, CaptchaError>;
 
 #[derive(thiserror::Error, Debug)]
@@ -137,6 +151,24 @@ pub enum CaptchaError {
 #[cfg(test)]
 mod test {
     use nalgebra::{self as na, Const};
+
+    use crate::TxtClickCaptchaBuilder;
+
+    #[test]
+    fn gen_save() {
+        let c = "你好啊世界".chars();
+        let img = TxtClickCaptchaBuilder::new()
+            .set_total_chars(c)
+            .set_sel_chars(&[1, 3, 4])
+            .unwrap()
+            .set_txt_size(100)
+            .set_box_size(150, 150)
+            .rgba8()
+            .unwrap();
+        img.buf
+            .save_with_format("/root/document/captcha/demo.png", image::ImageFormat::Png)
+            .unwrap();
+    }
 
     #[test]
     fn test_rotation() {
